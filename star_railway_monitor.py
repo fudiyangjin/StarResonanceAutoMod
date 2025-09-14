b399e517926eaed72191898f203cebf98428801b
fudiyangjin <578306088@qq.com>
2025-09-14 21:18:33 +0800

    ver: 1.5

--- star_railway_monitor.py
+++ star_railway_monitor.py
@@ -209,7 +209,7 @@ def main():
     else:
         # 交互式选择
         print("星痕共鸣模组筛选器!")
-        print("版本: V1.4")
+        print("版本: V1.5")
         print("GitHub: https://github.com/fudiyangjin/StarResonanceAutoMod")
         print()
         
